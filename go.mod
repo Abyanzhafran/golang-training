e0885bb8f237c2256c23b17db759358155c7d18e
Zhafran <zhafranjupleks5758@gmail.com>
2024-06-21 11:22:20 +0700

    init commit, add unit test package

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module golang-advance
 go 1.22.2
 
 require (
+	github.com/DATA-DOG/go-sqlmock v1.5.2 // indirect
 	github.com/bytedance/sonic v1.11.8 // indirect
 	github.com/bytedance/sonic/loader v0.1.1 // indirect
 	github.com/cloudwego/base64x v0.1.4 // indirect
@@ -27,6 +28,8 @@ require (
 	github.com/jackc/pgx/v5 v5.6.0 // indirect
 	github.com/jackc/puddle v1.2.1 // indirect
 	github.com/jackc/puddle/v2 v2.2.1 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.8 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
@@ -48,4 +51,6 @@ require (
 	golang.org/x/text v0.16.0 // indirect
 	google.golang.org/protobuf v1.34.2 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	gorm.io/driver/postgres v1.5.9 // indirect
+	gorm.io/gorm v1.25.10 // indirect
 )
--- main.go
+++ main.go
@@ -4,33 +4,56 @@ import (
 	"context"
 	"log"
 
+	"golang-advance/entity"
 	"golang-advance/handler"
 	"golang-advance/repository/postgres_pgx"
+	postgres_gorm "golang-advance/repository/postrges_gorm"
 	"golang-advance/router"
 	"golang-advance/service"
 
 	"github.com/gin-gonic/gin"
 	"github.com/jackc/pgx/v5/pgxpool"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 func main() {
 	gin.SetMode(gin.ReleaseMode)
 	r := gin.Default()
 
-	pgxPool, err := connectDB("postgresql://postgres:postgres@localhost:5432/golang-training-db")
+	// Setup database connection
+	//dsn := "postgresql://postgres:postgres@localhost:5432/postgres"
+	// setup pgx connection
+	//pgxPool, err := connectDB(dsn)
+	//if err != nil {
+	//	log.Fatalln(err)
+	//}
+
+	// setup gorm connectoin
+	dsn := "postgresql://postgres:postgres@localhost:5432/golang-training-db"
+	gormDB, err := gorm.Open(postgres.Open(dsn), &gorm.Config{SkipDefaultTransaction: true})
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	// pgx db is enabled. comment to disabled
-	userRepo := postgres_pgx.NewUserRepository(pgxPool)
+	// Setup auto migration
+	if err := gormDB.AutoMigrate(&entity.User{}); err != nil {
+		log.Fatal(err.Error())
+	}
+
+	// slice db is disabled. uncomment to enabled
+	// var mockUserDBInSlice []entity.User
+	// _ = slice.NewUserRepository(mockUserDBInSlice)
+
+	// uncomment to use postgres pgx
+	// userRepo := postgres_pgx.NewUserRepository(pgxPool)
+
+	userRepo := postgres_gorm.NewUserRepository(gormDB)
 	userService := service.NewUserService(userRepo)
 	userHandler := handler.NewUserHandler(userService)
 
-	// Routes
 	router.SetupRouter(r, userHandler)
 
-	// Run the server
 	log.Println("Running server on port 8080")
 	r.Run(":8080")
 }
